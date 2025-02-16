698b78424f0226e33e0edf20eed7a57b66b5d315
Ahmad Salemeh <128619518+black-beard2002@users.noreply.github.com>
2025-02-16 15:46:21 +0200

    Update page.tsx

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,48 +1,40 @@
 import {
-  getProfileByUsername,
-  getUserLikedPosts,
-  getUserPosts,
-  isFollowing,
-} from "@/actions/profile.action";
-import { notFound } from "next/navigation";
-import { Metadata } from "next";
-import ProfilePageClient from "./ProfilePageClient";
-
-type Props = {
-  params: {
-    username: string;
-  };
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const user = await getProfileByUsername(params.username);
-  if (!user) return {};
-
-  return {
-    title: `${user.name ?? user.username}`,
-    description: user.bio || `Check out ${user.username}'s profile.`,
-  };
-}
-
-async function ProfilePageServer({ params }: Props) {
-  const user = await getProfileByUsername(params.username);
-
-  if (!user) notFound();
-
-  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
-    getUserPosts(user.id),
-    getUserLikedPosts(user.id),
-    isFollowing(user.id),
-  ]);
-
-  return (
-    <ProfilePageClient
-      user={user}
-      posts={posts}
-      likedPosts={likedPosts}
-      isFollowing={isCurrentUserFollowing}
-    />
-  );
-}
-
-export default ProfilePageServer;
+    getProfileByUsername,
+    getUserLikedPosts,
+    getUserPosts,
+    isFollowing,
+  } from "@/actions/profile.action";
+  import { notFound } from "next/navigation";
+  import ProfilePageClient from "./ProfilePageClient";
+  
+  export async function generateMetadata({ params }: { params: { username: string } }) {
+    const user = await getProfileByUsername(params.username);
+    if (!user) return;
+  
+    return {
+      title: `${user.name ?? user.username}`,
+      description: user.bio || `Check out ${user.username}'s profile.`,
+    };
+  }
+  
+  async function ProfilePageServer({ params }: { params: { username: string } }) {
+    const user = await getProfileByUsername(params.username);
+  
+    if (!user) notFound();
+  
+    const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+      getUserPosts(user.id),
+      getUserLikedPosts(user.id),
+      isFollowing(user.id),
+    ]);
+  
+    return (
+      <ProfilePageClient
+        user={user}
+        posts={posts}
+        likedPosts={likedPosts}
+        isFollowing={isCurrentUserFollowing}
+      />
+    );
+  }
+  export default ProfilePageServer;
